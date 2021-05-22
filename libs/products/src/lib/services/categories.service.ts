91763bae8dd3524bb717d9b177cbbb6d8b0befcc
jignesh zala <jigneshkumar1494@gmail.com>
2021-05-22 14:05:19 +0530

    create category service in products lib

---
+++ libs/products/src/lib/services/categories.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+// import { environment } from '@env/environment';
+import { Category } from '../modles/category';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoriesService {
+  // apiURLCategories = environment.apiUrl + 'categories';
+
+  constructor(private http: HttpClient) {}
+
+  getCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>('localhost:3000/api/v1/categories');
+  }
+}
