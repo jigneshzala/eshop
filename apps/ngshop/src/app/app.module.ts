34dc49bc232991c06d4c2592c8a9d68079e03b26
jignesh zala <jigneshkumar1494@gmail.com>
2021-05-26 18:02:32 +0530

    products and category list

--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { ProductListComponent } from './pages/product-list/product-list.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { RouterModule, Routes } from '@angular/router';
@@ -19,16 +18,11 @@ const Routes: Routes = [
     path: '',
     component: HomePageComponent,
   },
-  {
-    path: 'product',
-    component: ProductListComponent,
-  },
 ];
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    ProductListComponent,
     HeaderComponent,
     FooterComponent,
     NavComponent,
--- libs/products/src/lib/products.module.ts
+++ libs/products/src/lib/products.module.ts
@@ -3,23 +3,50 @@ import { CommonModule } from '@angular/common';
 import { ProductsSearchComponent } from './components/products-search/products-search.component';
 import { CategoriesBannerComponent } from './components/categories-banner/categories-banner.component';
 // import { OrdersModule } from '@eshop-team/orders';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { ProductItemComponent } from './components/product-item/product-item.component';
 import { FeaturedProductsComponent } from './components/featured-products/featured-products.component';
+import { ProductsListComponent } from './pages/products-list/products-list.component';
+import { CheckboxModule } from 'primeng/checkbox';
+import { FormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  {
+    path: 'products',
+    component: ProductsListComponent,
+  },
+  {
+    path: 'category/:categoryid',
+    component: ProductsListComponent,
+  },
+  /* {
+    path: 'products/:productid',
+    component: ProductPageComponent
+  } */
+];
+
 @NgModule({
-  imports: [CommonModule, RouterModule, ButtonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes),
+    ButtonModule,
+    CheckboxModule,
+  ],
   declarations: [
     ProductsSearchComponent,
     CategoriesBannerComponent,
     ProductItemComponent,
     FeaturedProductsComponent,
+    ProductsListComponent,
   ],
   exports: [
     ProductsSearchComponent,
     ProductItemComponent,
     FeaturedProductsComponent,
     CategoriesBannerComponent,
+    ProductsListComponent,
   ],
 })
 export class ProductsModule {}
