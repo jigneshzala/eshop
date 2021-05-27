d04695415dc034a9f10414a9e752c6a88f88da9e
jignesh zala <jigneshkumar1494@gmail.com>
2021-05-27 14:50:38 +0530

    update cart item quanitity

--- libs/orders/src/lib/orders.module.ts
+++ libs/orders/src/lib/orders.module.ts
@@ -5,15 +5,15 @@ import { BadgeModule } from 'primeng/badge';
 import { RouterModule, Routes } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { InputNumberModule } from 'primeng/inputnumber';
-// import { OrderSummaryComponent } from './components/order-summary/order-summary.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-// import { CheckoutPageComponent } from './pages/checkout-page/checkout-page.component';
+
 import { InputTextModule } from 'primeng/inputtext';
 import { InputMaskModule } from 'primeng/inputmask';
 import { DropdownModule } from 'primeng/dropdown';
 import { CartIconComponent } from './components/cart-icon/cart-icon.component';
 import { CartPageComponent } from './pages/cart-page/cart-page.component';
-// import { ThankYouComponent } from './pages/thank-you/thank-you.component';
+import { OrderSummaryComponent } from './components/order-summary/order-summary.component';
+
 const routes: Routes = [
   {
     path: 'cart',
@@ -42,20 +42,8 @@ const routes: Routes = [
     DropdownModule,
   ],
   providers: [],
-  declarations: [
-    /*
-    OrderSummaryComponent,
-    CheckoutPageComponent,
-    ThankYouComponent */
-
-    CartIconComponent,
-    CartPageComponent,
-  ],
-  exports: [
-    CartIconComponent,
-    CartPageComponent,
-    //  OrderSummaryComponent
-  ],
+  declarations: [CartIconComponent, CartPageComponent, OrderSummaryComponent],
+  exports: [CartIconComponent, CartPageComponent, OrderSummaryComponent],
 })
 export class OrdersModule {
   constructor(cartService: CartService) {
